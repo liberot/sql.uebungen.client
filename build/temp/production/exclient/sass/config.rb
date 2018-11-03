4b7905ab01b7f06f6772d54acbbea277426844bf
liberot <vberzsin@web.de>
2018-11-03 13:11:58 +0100

    initial

---
+++ build/temp/production/exclient/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = '/Users/liberot/Workspace/SQLExercises/sql.uebungen/excercises.client/build/.sass-cache'
---
+++ build/temp/production/exclient/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/liberot/Workspace/SQLExercises/sql.uebungen/excercises.client/app.js
